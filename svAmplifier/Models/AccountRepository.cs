6344141d8e5ee91fa60753a4af439934232f2a62
CMedberg <cristianmedberg@gmail.com>
2017-12-05 15:38:51 +0100

    _Layout nästan klar

--- svAmplifier/Models/AccountRepository.cs
+++ svAmplifier/Models/AccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using svAmplifier.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace svAmplifier.Models
 {
     public class AccountRepository
     {
-        public List<User>
+        public List<User> listUsers;
     }
 }
--- svAmplifier/Startup.cs
+++ svAmplifier/Startup.cs
@@ -15,6 +15,7 @@ namespace svAmplifier
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,6 +24,9 @@ namespace svAmplifier
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMvcWithDefaultRoute();
+                app.UseStaticFiles();
+
             }
 
             app.Run(async (context) =>
