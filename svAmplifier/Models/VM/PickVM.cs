e6e1611300d0f85511078cad033d5905b3f4017b
CMedberg <cristianmedberg@gmail.com>
2017-12-05 13:37:42 +0100

    PickVM MarketItemVM

---
+++ svAmplifier/Models/VM/PickVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace svAmplifier.Models.VM
+{
+    public class PickVM
+    {
+        public Location PickLocation { get; set; }
+        public Mushroom PickedMushroom { get; set; }
+        public double Weight { get; set; }
+        public DateTime PickDate { get; set; }
+        public int UserId { get; set; }
+    }
+}
