360bf4e82d4330f044dc2d98edef17eb4d16de4c
Daniel Söderberg <mr.daniel.soderberg@gmail.com>
2017-12-05 14:51:41 +0100

    test repository

--- svAmplifier/Models/AccountRepository.cs
+++ svAmplifier/Models/AccountRepository.cs
@@ -8,31 +8,73 @@ namespace svAmplifier.Models
 {
     public class AccountRepository
     {
-        public User[] userList;
-
-        public AccountRepository()
+        static List<User> users = new List<User>
         {
-            userList = new User[]
-            {
-                new User
+            new User {
+                UserName = "Danne",
+                Email = "danne@danne.com",
+                Password = "password",
+                Address = new Address
                 {
-                    UserName = "Daniel",
-                    Clan = "",
-                    Address = ""
-                },
-                new User
-                {
-                    UserName = "Cristian",
-                    Clan = "",
-                    Address = ""
-                },
-                new User
-                {
-                    UserName = "Pontus",
-                    Clan = "",
-                    Address = ""
+                    City = "Sthlm",
+                    Street = "Hornstull",
+                    Zipcode = "12344"
                 }
-            };
+            },
+            new User {
+                UserName = "Pontus",
+                Email = "pontus@pontus.com",
+                Password = "password",
+                Address = new Address
+                {
+                    City = "Sthlm",
+                    Street = "Kistagatan",
+                    Zipcode = "16533"
+                }
+            },
+            new User {
+                UserName = "Håkan",
+                Email = "hakan@hakan.com",
+                Password = "password",
+                Address = new Address
+                {
+                    City = "Sollentuna",
+                    Street = "Sollentunagatan",
+                    Zipcode = "12452"
+                }
+            },
+            new User {
+                UserName = "Cristian",
+                Email = "cristian@cristian.com",
+                Password = "password",
+                Address = new Address
+                {
+                    City = "Solna",
+                    Street = "Solnagatan",
+                    Zipcode = "1113"
+                }
+            },
+            new User {
+                UserName = "Patrick",
+                Email = "patrik@patrik.com",
+                Password = "password",
+                Address = new Address
+                {
+                    City = "Malmö",
+                    Street = "Malmögatan",
+                    Zipcode = "22344"
+                }
+            }
+        };
+
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        public User[] GetAllUsers()
+        {
+            return users.ToArray();
         }
     }
 }
