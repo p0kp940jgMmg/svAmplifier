303e71f66610cb9638322e916578d5c81665323f
Daniel Söderberg <mr.daniel.soderberg@gmail.com>
2017-12-05 16:55:23 +0100

    login databas

--- svAmplifier/Controllers/HomeController.cs
+++ svAmplifier/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using svAmplifier.Models;
+using svAmplifier.Models.Entities;
 using svAmplifier.Models.VM;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,6 +13,13 @@ namespace svAmplifier.Controllers
 {
     public class HomeController : Controller
     {
+        AccountRepository accountRepository;
+
+        public HomeController(AccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -18,16 +27,29 @@ namespace svAmplifier.Controllers
 
         //i formulär: alltid en get och en post. en som visar... 
         [HttpGet]
-        public IActionResult Login()
+        public async Task<IActionResult> Login()            
         {
+            await accountRepository.AddUser(new RegisterUserVM { UserName = "danne", Password="Password_123"});
             return View();
         }
 
         [HttpPost]
-        public IActionResult Login(LoginVM loginVM)
+        public async Task<IActionResult> Login(LoginVM loginVM)
         {
-            return View();
-            //daebjkfc-lasndc
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var temp = await accountRepository.Login(loginVM);
+
+            if (!temp)
+            {
+                return View(loginVM);
+            }
+
+            return RedirectToAction(nameof(Index),nameof(User));
+
         }
     }
 }
