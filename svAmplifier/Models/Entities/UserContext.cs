5c42799ae40bdd2e369ed36acdb630ba1d073760
Daniel Söderberg <mr.daniel.soderberg@gmail.com>
2017-12-07 10:18:29 +0100

    DbContext UserContext Meckat i SCSS

---
+++ svAmplifier/Models/Entities/UserContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace svAmplifier.Models.Entities
+{
+    public partial class UserContext : DbContext
+    {
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<Pick> Pick { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WebApplication1DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Email);
+
+                entity.Property(e => e.UserName);
+
+                entity.Property(e => e.Password);
+
+                entity.Property(e => e.Address);
+            });
+
+            modelBuilder.Entity<Pick>(entity =>
+            {
+                entity.Property(e => e.Mushroom);
+
+                entity.Property(e => e.User);
+
+                entity.Property(e => e.Location);
+            });
+        }
+    }
+}
