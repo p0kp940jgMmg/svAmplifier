68549e06059dab120c251282cf1678239a77f460
Daniel Söderberg <mr.daniel.soderberg@gmail.com>
2017-12-07 14:52:19 +0100

    vymodell

--- svAmplifier/Models/AccountRepository.cs
+++ svAmplifier/Models/AccountRepository.cs
@@ -22,64 +22,64 @@ namespace svAmplifier.Models
             this.userManager = userManager;
             this.signInManager = signInManager;
         }
-        static List<User> users = new List<User>
-        {
-            new User {
-                UserName = "Danne",
-                Email = "danne@danne.com",
-                Password = "password",
-                Address = new Address
-                {
-                    City = "Sthlm",
-                    Street = "Hornstull",
-                    Zipcode = "12344"
-                }
-            },
-            new User {
-                UserName = "Pontus",
-                Email = "pontus@pontus.com",
-                Password = "password",
-                Address = new Address
-                {
-                    City = "Sthlm",
-                    Street = "Kistagatan",
-                    Zipcode = "16533"
-                }
-            },
-            new User {
-                UserName = "Håkan",
-                Email = "hakan@hakan.com",
-                Password = "password",
-                Address = new Address
-                {
-                    City = "Sollentuna",
-                    Street = "Sollentunagatan",
-                    Zipcode = "12452"
-                }
-            },
-            new User {
-                UserName = "Cristian",
-                Email = "cristian@cristian.com",
-                Password = "password",
-                Address = new Address
-                {
-                    City = "Solna",
-                    Street = "Solnagatan",
-                    Zipcode = "1113"
-                }
-            },
-            new User {
-                UserName = "Patrick",
-                Email = "patrik@patrik.com",
-                Password = "password",
-                Address = new Address
-                {
-                    City = "Malmö",
-                    Street = "Malmögatan",
-                    Zipcode = "22344"
-                }
-            }
-        };
+        //static List<User> users = new List<User>
+        //{
+        //    new User {
+        //        UserName = "Danne",
+        //        Email = "danne@danne.com",
+        //        Password = "password",
+        //        Address = new Address
+        //        {
+        //            City = "Sthlm",
+        //            Street = "Hornstull",
+        //            Zipcode = "12344"
+        //        }
+        //    },
+        //    new User {
+        //        UserName = "Pontus",
+        //        Email = "pontus@pontus.com",
+        //        Password = "password",
+        //        Address = new Address
+        //        {
+        //            City = "Sthlm",
+        //            Street = "Kistagatan",
+        //            Zipcode = "16533"
+        //        }
+        //    },
+        //    new User {
+        //        UserName = "Håkan",
+        //        Email = "hakan@hakan.com",
+        //        Password = "password",
+        //        Address = new Address
+        //        {
+        //            City = "Sollentuna",
+        //            Street = "Sollentunagatan",
+        //            Zipcode = "12452"
+        //        }
+        //    },
+        //    new User {
+        //        UserName = "Cristian",
+        //        Email = "cristian@cristian.com",
+        //        Password = "password",
+        //        Address = new Address
+        //        {
+        //            City = "Solna",
+        //            Street = "Solnagatan",
+        //            Zipcode = "1113"
+        //        }
+        //    },
+        //    new User {
+        //        UserName = "Patrick",
+        //        Email = "patrik@patrik.com",
+        //        Password = "password",
+        //        Address = new Address
+        //        {
+        //            City = "Malmö",
+        //            Street = "Malmögatan",
+        //            Zipcode = "22344"
+        //        }
+        //    }
+        //}
 
 
         //public async Task<bool> AddUser(RegisterUserVM regVm)
@@ -112,9 +112,9 @@ namespace svAmplifier.Models
             return result.Succeeded;
         }
 
-        public User[] GetAllUsers()
-        {
-            return users.ToArray();
-        }
+        //public User[] GetAllUsers()
+        //{
+        //    return users.ToArray();
+        //}
     }
 }
--- svAmplifier/Models/VM/PickVM.cs
+++ svAmplifier/Models/VM/PickVM.cs
@@ -8,10 +8,17 @@ namespace svAmplifier.Models.VM
 {
     public class PickVM
     {
-        public Location PickLocation { get; set; }
-        public Mushroom PickedMushroom { get; set; }
-        public double Weight { get; set; }
-        public DateTime PickDate { get; set; }
-        public int UserId { get; set; }
-    }
+        //public Location PickLocation { get; set; }
+        //public Mushroom PickedMushroom { get; set; }
+        //public double Weight { get; set; }
+        //public DateTime PickDate { get; set; }
+        //public int UserId { get; set; }
+
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
+        public string MushroomName { get; set; }
+        public string MushroomPicUrl { get; set; }
+        public DateTime DatePicked { get; set; }
+        }
 }
